1a39ff04e748c64744881457e3dcd792696de0d7
strhzy <alexeydm2@gmail.com>
2025-07-18 21:13:41 +0300

    attacks update 2

--- Models/Attack.cs
+++ Models/Attack.cs
@@ -21,13 +21,10 @@ public class Attack
     public Guid? EnemyId { get; set; }
 
     [StringLength(1000)]
-    [JsonPropertyName("desc")]
     public string Description { get; set; } = string.Empty;
-
-    [JsonPropertyName("attack_bonus")]
+    
     public int? AttackBonus { get; set; }
 
     [StringLength(50)]
-    [JsonPropertyName("damage_dice")]
     public string DamageDice { get; set; } = string.Empty;
 }
