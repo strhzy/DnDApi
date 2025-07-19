ce19826f34656da8128aca86f7184b35e1cbe4a9
strhzy <alexeydm2@gmail.com>
2025-07-20 00:18:55 +0300

    test

--- Program.cs
+++ Program.cs
@@ -1,11 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using DnDAPI.Models;
 using DnDAPI.Controllers;
-using QuestPDF.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
-
-QuestPDF.Settings.License = LicenseType.Community;
 
 // Add services to the container.
 builder.Services.AddControllers();
