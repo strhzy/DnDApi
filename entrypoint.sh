2bca70389204cb3c9e32b5aa37c15f9c26a33d72
strhzy <alexeydm2@gmail.com>
2025-09-26 21:56:34 +0300

    change script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,6 @@
 ﻿#!/bin/bash
 set -e
 
-echo "⏳ Ждём базу данных..."
-# Можно добавить healthcheck/wait-for-it, если надо ждать postgres/mysql
-# пример для postgres:
-# until pg_isready -h db -p 5432 -U $POSTGRES_USER; do
-#   sleep 2
-# done
-
-echo "📦 Применяем миграции..."
+dotnet ef migrations add init
 dotnet ef database update --no-build --project DnDAPI.csproj
-
-echo "🚀 Запускаем приложение..."
 exec dotnet DnDAPI.dll
